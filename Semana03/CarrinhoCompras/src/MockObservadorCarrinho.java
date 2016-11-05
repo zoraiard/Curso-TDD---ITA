9dccbb92eb526b14bfaeb1e4fa9fe8fc12d7146c
Zoraia Rodrigues <zoraiarodrigues@gmail.com>
2016-11-05 16:00:12 -0200

    Aulas da Semana 03 - Mock Object

---
+++ Semana03/CarrinhoCompras/src/MockObservadorCarrinho.java
@@ -0,0 +1,30 @@
+
+import static org.junit.Assert.*;
+
+import javax.management.RuntimeErrorException;
+
+public class MockObservadorCarrinho implements ObservadorCarrinho {
+
+	private String nomeRecebido;
+	private int valorRecebido;
+	private boolean darPau = false;
+
+	@Override
+	public void prudutoAdicionado(String nome, int valor) {
+		if (darPau) 
+			throw new RuntimeException( "Problema simulado pelo mock");
+			nomeRecebido = nome;
+			valorRecebido = valor;
+	}
+
+	public void verificaRecebimentoProduto(String nomeEsperado, int valorEsperado) {
+		assertEquals(nomeEsperado, nomeRecebido);
+		assertEquals(valorEsperado, valorRecebido);
+
+	}
+
+	public void queroQueVoceDePau() {
+		darPau = true;
+	}
+
+}
